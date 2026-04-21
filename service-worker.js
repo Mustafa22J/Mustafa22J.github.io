f56443c0414b69ea0c57b9a74af30a03d4bb8208
Mustafa Jawish <ender201619@gmail.com>
2026-04-21 01:10:01 -0400

    Update cache version

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'mj-portfolio-v4';
+const CACHE_NAME = 'mj-portfolio-v5';
 const urlsToCache = ['/', '/index.html', '/styles.css', '/script.js', '/manifest.json', '/Mustafa.png', '/Mustafa_Jawish_IT_Networking_CV.pdf'];
 
 self.addEventListener('install', event => { event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))); });
